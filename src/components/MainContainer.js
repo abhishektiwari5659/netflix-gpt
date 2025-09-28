2e1fc97ef3dc072485b15844da9aa453e3e7cd81
HeheAbhi <abhishekti5659@gmail.com>
2025-09-28 15:35:47 +0530

    updated styling in main and secondary containers

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,20 +1,22 @@
-
 import { useSelector } from 'react-redux'
 import VideoTitle from "./VideoTitle"
 import VideoBackground from "./VideoBackground"
 
 const MainContainer = () => {
     const movies = useSelector(store => store.movies.nowPlayingMovies)
-    if(!movies) return
+    if(!movies) return null
+
     const mainMovies = movies[0];
-    const {title, overview, id} = mainMovies;
-  return (
-    <div>
-        <VideoTitle title={title} overview={overview}> </VideoTitle>
-        <VideoBackground movieId={id}/>
-      
-    </div>
-  )
+    const { title, overview, id } = mainMovies;
+
+    return (
+        <div className='bg-black w-full'>
+            <div className="px-4 sm:px-6 md:px-10 lg:px-8 xl:px-8 py-6 sm:py-8 md:py-10 lg:py-8 xl:py-8">
+                <VideoTitle title={title} overview={overview} />
+                <VideoBackground movieId={id} />
+            </div>
+        </div>
+    )
 }
 
 export default MainContainer
